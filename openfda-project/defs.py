2d09e4f635119dc2792fd2f619e87b9d81a6e257
IreneTorrijos <torrijos.irene@gmail.com>
2018-04-29 20:12:45 +0200

    final project

--- openfda-project/defs.py
+++ openfda-project/defs.py
@@ -1,58 +1,35 @@
+def list_man():
 
-
-def act_ing:
     headers = {'User-Agent': 'http-client'}
     conn = http.client.HTTPSConnection("api.fda.gov")
-    data = self.path.strip('/search?').split('&')
-    drug = data[0].split('=')[1]
+    data = self.path.strip('/search?').split('?')
     limit = data[1].split('=')[1]
-    print("client has succesfully made a request")
+    print("Searching . . .")
 
-    url = "/drug/label.json?search=active_ingredient:" + drug + '&' + 'limit=' + limit
-    print(url)
+    url = "/drug/label.json?limit=" + limit
     conn.request("GET", url, None, headers)
     r1 = conn.getresponse()
     repos_raw = r1.read().decode("utf-8")
     conn.close()
     repos = json.loads(repos_raw)
-    self.wfile.write(bytes(json.dumps(repos), "utf8"))
 
-    active = []
+    nlimit = int(limit)
     a = 0
+    druglist = []
 
-    while a < limit:
-        if 'active_ingredient' in repos['results'][a]:
+    while a < nlimit:
+        try:
+            druglist.append(repos['results'][a]["openfda"]["manufacturer_name"][0])
             a += 1
-            mydrugs.append(repos['results'][a]['id'])
-        else:
-            a += 1
-            mydrugs.append("No drug found in this index")
 
-    with open("active_ingredient.html", "w") as f:
-        f.write("<head>" + "DRUGS' ID LIST" + "</head>")
-        f.write("<ol>" + "\n")
-        for element in mydrugs:
+        except KeyError:
+            druglist.append('No brand name found in this index')
+            a += 1
+
+    with open("manufacturer_list.html", "w") as f:
+        f.write('<body style="background-color:palegreen">')
+        f.write("<head>" + "<h1>" + "DRUG'S LIST" + "</h1>" + "</head>")
+        f.write("<ol>")
+        for element in druglist:
             element_1 = "<t>" + "<li>" + element
             f.write(element_1)
-
-    with open("web3.html", "r") as f:
-        file = f.read()
-
-    web_contents = file
-    web_headers = "HTTP/1.1 200"
-    web_headers += "\n" + "Content-Type: text/html"
-    web_headers += "\n" + "Content-Length: %i" % len(str.encode(web_contents))
-    clientsocket.send(str.encode(web_headers + "\n\n" + web_contents))
-    clientsocket.close()
-
-
-
-
-
-
-#if 'brand_name' in repos['results'][a]['openfda']:
-                    #a += 1
-                    #active.append(repos['results'][a]['openfda']['brand_name'][0])
-                #else:
-                    #a += 1
-                    #active.append("No brand found in this index")
--- openfda-project/server.py
+++ openfda-project/server.py
@@ -53,8 +53,113 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                     element_1 = "<t>" + "<li>" + element
                     f.write(element_1)
 
+        def man_name():
+            headers = {'User-Agent': 'http-client'}
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            data = self.path.strip('/search?').split('&')
+            manufacturer = data[0].split('=')[1]
+            limit = data[1].split('=')[1]
+            print("Searching . . .")
+
+            url = "/drug/label.json?search=openfda.manufacturer_name:" + manufacturer + '&' + 'limit=' + limit
+            conn.request("GET", url, None, headers)
+            r1 = conn.getresponse()
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
+            repos = json.loads(repos_raw)
+
+            manuf = []
+            a = 0
+            nlimit = int(limit)
+
+            while a < nlimit:
+                try:
+                    manuf.append(repos['results'][a]["openfda"]["brand_name"][0])
+                    a += 1
+
+                except KeyError:
+                    manuf.append('No brand name found in this index')
+                    a += 1
+
+            with open("manufacturer_name.html", "w") as f:
+                f.write('<body style="background-color:lavender">')
+                f.write("<head>" + "<h1>" + "DRUG'S BRAND NAMES" + "</h1>" + "</head>")
+                f.write("<ol>")
+                for element in manuf:
+                    element_1 = "<t>" + "<li>" + element
+                    f.write(element_1)
+
+        def list_drug():
+
+            headers = {'User-Agent': 'http-client'}
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            limit = self.path.split("=")[1]
+            print("Searching . . .")
+
+            url = "/drug/label.json?limit=" + limit
+
+            conn.request("GET", url, None, headers)
+            r1 = conn.getresponse()
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
+            repos = json.loads(repos_raw)
 
 
+            nlimit = int(limit)
+            a = 0
+            druglist = []
+
+            while a < nlimit:
+                try:
+                    druglist.append(repos['results'][a]["openfda"]["brand_name"][0])
+                    a += 1
+
+                except KeyError:
+                    druglist.append('No brand name found in this index')
+                    a += 1
+
+            with open("drug_list.html", "w") as f:
+                f.write('<body style="background-color:#E9C2B7">')
+                f.write("<head>" + "<h1>" + "DRUGS LIST" + "</h1>" + "</head>")
+                f.write("<ol>")
+                for element in druglist:
+                    element_1 = "<t>" + "<li>" + element
+                    f.write(element_1)
+
+        def list_man():
+
+            headers = {'User-Agent': 'http-client'}
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            limit = self.path.split("=")[1]
+            print("Searching . . .")
+
+            url = "/drug/label.json?limit=" + limit
+            conn.request("GET", url, None, headers)
+            r1 = conn.getresponse()
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
+            repos = json.loads(repos_raw)
+
+            nlimit = int(limit)
+            a = 0
+            druglist = []
+
+            while a < nlimit:
+                try:
+                    druglist.append(repos['results'][a]["openfda"]["manufacturer_name"][0])
+                    a += 1
+
+                except KeyError:
+                    druglist.append('No manufacturer name found in this index')
+                    a += 1
+
+            with open("manufacturer_list.html", "w") as f:
+                f.write('<body style="background-color:#FFFFCC">')
+                f.write("<head>" + "<h1>" + "MANUFACTURERS LIST" + "</h1>" + "</head>")
+                f.write("<ol>")
+                for element in druglist:
+                    element_1 = "<t>" + "<li>" + element
+                    f.write(element_1)
 
 
         path = self.path
@@ -73,7 +178,26 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                 self.wfile.write(bytes(mensaje, "utf8"))
 
 
+        elif 'search_manufacturer' in self.path:
+            print('Manufacturer name')
+            man_name()
+            with open("manufacturer_name.html", "r") as f:
+                mensaje = f.read()
+                self.wfile.write(bytes(mensaje, "utf8"))
 
+        elif 'search_drug' in self.path:
+            print('Drug list')
+            list_drug()
+            with open("drug_list.html", "r") as f:
+                mensaje = f.read()
+                self.wfile.write(bytes(mensaje, "utf8"))
+
+        elif 'search_manlist' in self.path:
+            print('Manufacturer list')
+            list_man()
+            with open("manufacturer_list.html", "r") as f:
+                mensaje = f.read()
+                self.wfile.write(bytes(mensaje, "utf8"))
         return
 
 
@@ -85,12 +209,11 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 Handler = testHTTPRequestHandler
 
 httpd = socketserver.TCPServer((IP, PORT), Handler)
-print("serving at port", PORT)
+print("sconnected to", IP, PORT)
 try:
     httpd.serve_forever()
 except KeyboardInterrupt:
     pass
 
 httpd.server_close()
-print("")
 print("Server stopped!")
